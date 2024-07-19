8f20dff78154f0534107c63941025c1cb377922b
Connor Kress <con2coool@gmail.com>
2024-07-19 02:24:02 -0400

    Removed unnecessary API endpoint.

--- src/app/textbooks/page.tsx
+++ src/app/textbooks/page.tsx
@@ -1,3 +1,5 @@
+import { promises as fs } from "fs"
+import path from "path"
 import Textbook from '@/types/Textbook';
 import Link from 'next/link'
 
@@ -6,16 +8,24 @@ export const metadata = {
   description: "A list of available textbooks to post and view question answers.",
 };
 
-export default async function TextbookList() {
-  const res = await fetch("http://localhost:3000/textbooks/api", {
-    cache: "no-store",
-  });
-  const textbookNames: Textbook[] = await res.json();
+async function fetchTextbooks() {
+  const pdfDir = path.join(process.cwd(), "public", "pdf");
+  const fileNames = await fs.readdir(pdfDir);
+  return fileNames.map(fn => ({
+    fileName: fn,
+    filePath: `/pdf/${fn}`,
+    uriName: fn.replace(/.pdf$/, ""),
+    name: decodeURI(fn.replace(/.pdf$/, "")),
+  }));
+}
+
+export default async function TextbookListPage() {
+  const textbooks: Textbook[] = await fetchTextbooks();
   return (
     <>
       <h1>Available Textbooks:</h1>
       {
-        textbookNames.map((tb, i) =>
+        textbooks.map((tb, i) =>
           <Link href={`/textbooks/${tb.uriName}`} key={i}><h3>{tb.name}</h3></Link>
         )
       }
