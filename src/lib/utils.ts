4b1898f75f855dbbf04efa25cc8f99b08600349a
Connor Kress <con2coool@gmail.com>
2024-07-20 22:36:13 -0400

    Mock chapter and question data fetching

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,20 @@
 import { promises as fs } from "fs"
 import path from "path"
-import TextbookInfo from '@/types/TextbookInfo';
+import { TextbookInfo } from '@/types/TextbookInfo';
+import { mockChapterData } from "./mockData";
 
 export async function fetchTextbookInfoList(): Promise<TextbookInfo[]> {
   const pdfDir = path.join(process.cwd(), "public", "pdf");
   const fileNames = await fs.readdir(pdfDir);
-  return fileNames.map(fn => ({
-    fileName: fn,
-    filePath: `/pdf/${fn}`,
-    uriName: fn.replace(/.pdf$/, ""),
-    name: decodeURI(fn.replace(/.pdf$/, "")),
-  }));
+  const uriNames = fileNames.map(name => name.replace(/.pdf$/, ""));
+  return await Promise.all(uriNames.map(name => fetchTextbookInfo(name)));
 }
 
 export async function fetchTextbookInfo(
   uriName: string
 ): Promise<TextbookInfo> {
   return {
+    chapters: mockChapterData,
     fileName: `${uriName}.pdf`,
     filePath: `/pdf/${uriName}.pdf`,
     uriName: uriName,
--- src/types/TextbookInfo.ts
+++ src/types/TextbookInfo.ts
@@ -1,8 +1,16 @@
-type TextbookInfo = {
+export type ChapterInfo = {
+  name: string,
+  num: number,
+  questions: {
+    id: number,
+    num: number,
+  }[],
+}
+
+export type TextbookInfo = {
+  chapters: ChapterInfo[],
   fileName: string,
   filePath: string,
   uriName: string,
   name: string,
 }
-
-export default TextbookInfo;
