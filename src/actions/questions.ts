6575b76d90ccae99bba667edf1cc77d8236efef8
Connor Kress <con2coool@gmail.com>
2024-07-20 22:33:34 -0400

    Mock data file

--- src/actions/questions.ts
+++ src/actions/questions.ts
@@ -1,64 +1,10 @@
 "use server";
 
+import { mockQuestionData } from "@/lib/mockData";
 import Question from "@/types/Question";
 
-export async function fetchQuestionData(questionId: number): Promise<Question> {
-  return {
-    id: questionId,
-    text: "Is this a question?",
-    comments: [
-      {
-        id: 0,
-        author: "Mandar",
-        postDate: new Date(),
-        likes: 4,
-        dislikes: 1,
-        text: "Hello, who is this?",
-        replies: [
-          {
-            id: 1,
-            author: "Minecraft Server",
-            postDate: new Date(),
-            likes: 4,
-            dislikes: 1,
-            text: "I am the SeRVeR.",
-          },
-          {
-            id: 2,
-            author: "Connor",
-            postDate: new Date(),
-            likes: 4,
-            dislikes: 1,
-            text: "Erm... what the sigma?",
-          },
-        ],
-      },
-      {
-        id: 3,
-        author: "Omar",
-        postDate: new Date(),
-        likes: 4,
-        dislikes: 1,
-        text: "I AM A DOCTOR-ENGINEER-POET. ".repeat(10),
-        replies: [
-          {
-            id: 4,
-            author: "Mo",
-            postDate: new Date(),
-            likes: 4,
-            dislikes: 1,
-            text: "Ok Omar, I believe you.",
-          },
-          {
-            id: 5,
-            author: "Pranav",
-            postDate: new Date(),
-            likes: 4,
-            dislikes: 1,
-            text: "Can I smell your toes?",
-          },
-        ],
-      },
-    ],
-  };
+export async function fetchQuestionData(
+  _questionId: number,
+): Promise<Question> {
+  return mockQuestionData;
 }
---
+++ src/lib/mockData.ts
@@ -0,0 +1,94 @@
+import Question from "@/types/Question";
+import { ChapterInfo } from "@/types/TextbookInfo";
+
+export const mockChapterData: ChapterInfo[] = [
+  {
+    name: "The first chapter",
+    num: 1,
+    questions: [
+      {
+        id: 1337,
+        num: 1,
+      },
+      {
+        id: 1338,
+        num: 2,
+      },
+    ],
+  },
+  {
+    name: "The second chapter",
+    num: 2,
+    questions: [
+      {
+        id: 1339,
+        num: 1,
+      },
+      {
+        id: 1340,
+        num: 2,
+      },
+    ],
+  },
+];
+
+export const mockQuestionData: Question = {
+    id: 123455555,
+    chapter: 3,
+    num: 10,
+    text: "Is this a question?",
+    comments: [
+      {
+        id: 0,
+        author: "Mandar",
+        postDate: new Date(),
+        likes: 4,
+        dislikes: 1,
+        text: "Hello, who is this?",
+        replies: [
+          {
+            id: 1,
+            author: "Minecraft Server",
+            postDate: new Date(),
+            likes: 4,
+            dislikes: 1,
+            text: "I am the SeRVeR.",
+          },
+          {
+            id: 2,
+            author: "Connor",
+            postDate: new Date(),
+            likes: 4,
+            dislikes: 1,
+            text: "Erm... what the sigma?",
+          },
+        ],
+      },
+      {
+        id: 3,
+        author: "Omar",
+        postDate: new Date(),
+        likes: 4,
+        dislikes: 1,
+        text: "I AM A DOCTOR-ENGINEER-POET. ".repeat(10),
+        replies: [
+          {
+            id: 4,
+            author: "Mo",
+            postDate: new Date(),
+            likes: 4,
+            dislikes: 1,
+            text: "Ok Omar, I believe you.",
+          },
+          {
+            id: 5,
+            author: "Pranav",
+            postDate: new Date(),
+            likes: 4,
+            dislikes: 1,
+            text: "Can I smell your toes?",
+          },
+        ],
+      },
+    ],
+  };
