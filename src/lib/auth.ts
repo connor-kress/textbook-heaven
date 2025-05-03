ebe35d7ed24a27fe7c1c327eb508e8ea7b4600b9
Connor Kress <ckress@ufl.edu>
2025-05-03 16:15:35 -0400

    added `nextCookies` better-auth plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
 import { Pool } from "pg";
- 
+
 export const auth = betterAuth({
+    plugins: [nextCookies()], // make sure nextCookies is the last plugin
     database: new Pool({
       user: process.env.DB_USER,
       host: process.env.DB_HOST,
