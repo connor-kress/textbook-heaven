86c360cc0f854a5fd5164a11aedbf4e5d70e42b5
Connor Kress <ckress@ufl.edu>
2025-05-05 20:52:50 -0400

    added drizzle packages and config file

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    user: process.env.DB_USER!,
+    host: process.env.DB_HOST!,
+    database: process.env.DB_NAME!,
+    password: process.env.DB_PASSWORD!,
+    port: process.env.DB_PORT === undefined
+      ? undefined
+      : parseInt(process.env.DB_PORT),
+    ssl: false,
+  },
+});
